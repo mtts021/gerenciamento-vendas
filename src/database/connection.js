b07a26510ec9400d2c336349a24685926ce42863
Matteus Santos S <matteus.py@gmail.com>
2024-05-13 22:35:32 -0300

    add eslint

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const connection = new Pool({connectionString: process.env.CONNECTION_STRING})
+// eslint-disable-next-line no-unused-vars
 connection.connect((err, client, release) => {
   if (err) {
     return console.error('Error acquiring client', err.stack)
